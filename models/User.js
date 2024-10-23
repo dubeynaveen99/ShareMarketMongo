33748c9dbd2d19deee2f977a881758636bf2fed9
Naveen Dubey <dubeynaveen99@gmail.com>
2024-10-23 12:33:37 +0530

    desing for landing page

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    balance: { type: Number, default: 1000000}
+    balance: { type: Number, default: 1000000},
+    investedAmount:{type:Number,default:0}
 });
 // Hash the password before saving
 UserSchema.pre('save', async function(next) {
